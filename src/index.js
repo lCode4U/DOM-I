6abef9518b97b0d3b533e5a9b9bdd21458a9f534
Torquoya Jackson <tlwilliams92tech@yahoo.com>
2021-04-01 05:21:23 -0700

    clobe

--- src/index.js
+++ src/index.js
@@ -13,20 +13,15 @@ const siteContent = {
   },
   "main-content": {
     "features-h4": "Features",
-    "features-content":
-      "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "about-h4": "About",
-    "about-content":
-      "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "services-h4": "Services",
-    "services-content":
-      "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "product-h4": "Product",
-    "product-content":
-      "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "vision-h4": "Vision",
-    "vision-content":
-      "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   contact: {
     "contact-h4": "Contact",
@@ -38,66 +33,77 @@ const siteContent = {
     copyright: "Copyright Great Idea! 2018",
   },
   img: {
-    "header-img":
-      "https://tk-assets.lambdaschool.com/d312ae1d-879c-4c0b-8e0a-d03661aed7d8_header-img.png",
-    "mid-page-img":
-      "https://tk-assets.lambdaschool.com/933e5c8a-f143-4a89-b81b-4a609c325f1a_mid-page-accent.jpg",
-    "logo-img":
-      "https://tk-assets.lambdaschool.com/d7b66b1d-1f0f-489e-b408-f8e25140c82c_logo.png",
+    "header-img": "https:tk-assets.lambdaschool.com/d312ae1d-879c-4c0b-8e0a-d03661aed7d8_header-img.png",
+    "mid-page-img": "https:tk-assets.lambdaschool.com/933e5c8a-f143-4a89-b81b-4a609c325f1a_mid-page-accent.jpg",
+    "logo-img": "https:tk-assets.lambdaschool.com/d7b66b1d-1f0f-489e-b408-f8e25140c82c_logo.png",
   },
 };
 
-// Example: Update the img src for the logo
+//Example: Update the img src for the logo
 
-// this is to select content
+//this is to select content
 const parentHead = document.querySelector("header");
 const logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["img"]["logo-img"])
-// console.log(logo);
-//console.log(parentHead);
+console.log(logo);
+console.log(parentHead);
 
 const parentCon = document.querySelector("div.container");
- //console.log(parentCon);
- 
+console.log(parentCon);
+
 const navCon = document.querySelectorAll("nav a");
 console.log(navCon);
- 
-  navCon.forEach((item, i)  => {
-   return item.textContent = siteContent["nav"];{'nav-item-${i +1'}}
-    );
 
-  //function navAnchors (navCon,anchorCallback) {
-    //return anchorCallback(navCon);
-  //}
- 
+navCon.forEach((item, i) => {
+  return item.textContent = siteContent["nav"]; {
+    'nav-item-${i +1'
+  }
+});
 
-
-   
+function navAnchors(navCon, anchorCallback) {
+  return anchorCallback(navCon);
+}
 
 
 
-  //document.textContent(siteContent.nav[length])
-//});
+
+
+
+
+// document.textContent(siteContent.nav[length]);
 // navCon[0].innerHTML = siteContent.nav[0];
 // navCon[1].innerHTML = siteContent.nav[1];
 // navCon[2].innerHTML = siteContent.nav[2];
 // navCon[3].innerHTML = siteContent.nav[3];
 // navCon[4].innerHTML = siteContent.nav[4];
 // navCon[5].innerHTML = siteContent.nav[5];
-//console.log(navCon);
+// console.log(navCon);
 
 
 
 const ctaCon = document.querySelector("section.cta");
+
+const ctaH4 = document.createElement("h4");
+const ctaP = document.createElement("p");
 console.log(ctaCon);
 
+
 // this is to push new elements to dom
-//const parentBody = document.createElement("div");
-//console.log(parentBody);
+const parentBody = document.createElement("div");
+console.log(parentBody);
 
 const mainCon = document.querySelector("section.main-content");
 console.log(mainCon);
 
+const topCon = document.querySelector("top-content");
+const topH4 = document.createElement("h4");
+const topP = document.createElement("p");
+console.log(topCon);
+
+const bottCon = document.querySelector(".bottom-content");
+const bottH4 = document.createElement("h4");
+const bottP = document.createElement("p");
+console.log(bottCon);
 
 const contactCon = document.querySelector("section.contact");
 console.log(contactCon);
@@ -108,43 +114,23 @@ console.log(footCon);
 
 
 
-// const headImg = document.getElementById("cta-img")
-// headImg.setAttribute('src', siteContent.img['header-img'])
-// console.log(headImg);
+const headImg = document.getElementById("cta-img")
+headImg.setAttribute('src', siteContent.img['header-img'])
+console.log(headImg);
 
-// const newTitle = document.querySelector("title");
-// console.log(newTitle);
+const newTitle = document.querySelector("title");
+console.log(newTitle);
 
-//  const ctaBut = document.querySelector('button');
-//  ctaBut.innerHTML = siteContent.cta.button;
-//  console.log(ctaBut);
+const ctaBut = document.querySelector('button');
+ctaBut.innerHTML = siteContent.cta.button;
+console.log(ctaBut);
 
-//  const midImg = document.querySelector(`.middle-img`)
-//  midImg.setAttribute('src', siteContent.img['mid-page-img'])
-//  console.log(midImg);
-
-// const ctaCon = document.querySelector(".cta-text");
-// const ctaH4 = document.createElement("h4");
-// const ctaP = document.createElement("p");
-// console.log(ctaCon);
-
-// const mainCon = document.getElementsByClassName("main-content");
-// console.log(mainCon);
-
-// const topCon = document.querySelector("main-content[1]");
-// const topH4 = document.createElement("h4");
-// const topP = document.createElement("p");
-// console.log(topCon);
-
-// const bottCon = document.querySelector(".bottom-content");
-// const bottH4 = document.createElement("h4");
-// const bottP = document.createElement("p");
-// console.log(bottCon);
-
-// //const navCon = mainCon.querySelectorAll("body header nav");
-// //const navCon = document.
-// //(siteContent.nav[length]);
-// //console.log(navCon);
+const midImg = document.querySelector(`.middle-img`)
+midImg.setAttribute('src', siteContent.img['mid-page-img'])
+console.log(midImg);
 
 
-
+// const navCon = mainCon.querySelectorAll("body header nav");
+// const navCon = document.
+//   (siteContent.nav[length]);
+// console.log(navCon);
